e72c612507dd0f09e4ae5a17ab25894b38f59b6f
Jonas Søndergaard <144332480+Fiskozo@users.noreply.github.com>
2024-01-16 10:27:35 +0100

    Add new Class on Hover

--- scripts.js
+++ scripts.js
@@ -3,15 +3,18 @@ container.classList.add('container');
 
 let numberOfGrid = 256
 
+
 const createGrid = function() {
     for (let index = 0; index < numberOfGrid; index++) {
         const newDiv = document.createElement('div');
         newDiv.classList.add('newDiv');
-        container.appendChild(newDiv); 
+        container.appendChild(newDiv);
+        newDiv.onmouseover = function() {
+            newDiv.classList.add('hover');
+      };     
     }
 } 
 createGrid();
 
-document.getElementById('newDiv').onmouseover = function() {
-      newDiv.classList('hover')
-};
+
+
