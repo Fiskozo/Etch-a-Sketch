6d4bf2e1ace011277ba24e7a5c34e38312d26252
Jonas Søndergaard <144332480+Fiskozo@users.noreply.github.com>
2024-01-16 14:55:36 +0100

    Fix size of grid layout

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 const container = document.querySelector('#container');
 container.classList.add('container');
 
-let numberOfGrid = 256
+let input = 16
+let numberOfGrid = input * input
+
 
 
 const createGrid = function() {
     for (let index = 0; index < numberOfGrid; index++) {
         const newDiv = document.createElement('div');
         newDiv.classList.add('newDiv');
+        newDiv.style.width = (500 / input) + "px"
+        newDiv.style.height = (500 / input) + "px"
         container.appendChild(newDiv);
         newDiv.onmouseover = function() {
             newDiv.classList.add('hover');
