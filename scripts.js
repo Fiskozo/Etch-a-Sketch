522a83654668dc3912c77f50613e99d899bbdf0d
Jonas Søndergaard <144332480+Fiskozo@users.noreply.github.com>
2024-01-19 09:32:22 +0100

    Add function so grid is reset if button is pressed

--- scripts.js
+++ scripts.js
@@ -8,6 +8,20 @@ let getInput = document.getElementById('btn').onclick = function() {
     let numberOfGrid = input * input
     if (input > 100) {
         alert("No more than 100 please :D");
+    }else if (container.hasChildNodes()) {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+          };
+        for (let index = 0; index < numberOfGrid; index++) {
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('newDiv');
+        newDiv.style.width = (500 / input) + "px"
+        newDiv.style.height = (500 / input) + "px"
+        container.appendChild(newDiv);
+        newDiv.onmouseover = function() {
+            newDiv.classList.add('hover');
+            };     
+        };
     }else{
     for (let index = 0; index < numberOfGrid; index++) {
         const newDiv = document.createElement('div');
